ecc6927e18a2bf62724b54cf40c2676b4b3689b3
funstra <founstra@gmail.com>
2021-11-23 21:59:18 +0100

    more stuff

--- index.js
+++ index.js
@@ -20,27 +20,29 @@ const flatten = arr => {
     }
     return { headers, rows: arr.map(_flatter) }
 }
-const users = []
-    ;
-(async () => {
-    const users = (await fetch('https://randomuser.me/api?results=10').then(res => res.json())).results
-    const flattendLocations = flatten(users.map(user => user.location))
+const fetchRender = async q => {
+    const users = (await fetch(`https://randomuser.me/api?results=${q ?? 20}`).then(res => res.json())).results
+    const flattendData = flatten(users.map(user => ({ ...user.name, ...user.location })))
+
 
     const table = document.querySelector('table')
     const tHeaderRow = table.tHead.rows[0]
     const tBody = table.tBodies[0]
 
-    const maxNumber = Math.max(...flattendLocations.rows.map(row => row.number))
+    tHeaderRow.innerHTML = ''
+    tBody.innerHTML = ''
+
+    const maxNumber = Math.max(...flattendData.rows.map(row => row.number))
 
 
-    flattendLocations.headers.forEach(col => {
+    flattendData.headers.forEach(col => {
         const headCell = document.createElement('th')
         headCell.scope = 'col'
         headCell.textContent = col
         tHeaderRow.appendChild(headCell)
     })
 
-    flattendLocations.rows.forEach(location => {
+    flattendData.rows.forEach(location => {
         const row = tBody.insertRow()
         if (location.number === maxNumber) {
             row.setAttribute('data-peak', 'true')
@@ -48,10 +50,15 @@ const users = []
         for (const key in location) {
             const cell = row.insertCell()
             cell.textContent = location[key]
+            cell.title = location[key]
         }
         row.style.setProperty('--color-sat', `${(location.number / maxNumber) * 100}%`)
-        row.style.setProperty('--color-hue', `${80 - (location.number / maxNumber) * 40}`)
+        row.style.setProperty('--color-hue', `${(location.number / maxNumber)}`)
     })
-})()
+}
+fetchRender()
 
 
+document.querySelector('.tableContainer input[type=range]').addEventListener('input', e => {
+    fetchRender(e.target.value)
+})
